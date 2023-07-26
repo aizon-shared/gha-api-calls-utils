228dbdfa3fc71a91e9baf69f80688a7c2e3decd9
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-07-26 19:17:57 +0200

    get-latest-tags testing

--- src/helpers/githubApi/getBranchCommit.ts
+++ src/helpers/githubApi/getBranchCommit.ts
@@ -1,8 +1,8 @@
-import { getOctokit } from "@actions/github";
+import { Octokit } from 'octokit';
 
 // It returns the commit sha of a branch (latest commit).
 export default async (token: string , owner: string, repo: string, branch: string) => {
-  const client = getOctokit(token);
+  const client = new Octokit({auth: token});
   const { data } = await client.rest.repos.getBranch({
     owner,
     repo,
--- src/helpers/githubApi/getTags.ts
+++ src/helpers/githubApi/getTags.ts
@@ -1,8 +1,8 @@
-import { getOctokit } from "@actions/github";
+import { Octokit } from 'octokit';
 
 // It returns the tags of a repository.
 export default async (token: string, owner: string, repo: string) => {
-  const client = getOctokit(token);
+  const client = new Octokit({auth: token});
   const { data } = await client.rest.repos.listTags({
     owner,
     repo,
