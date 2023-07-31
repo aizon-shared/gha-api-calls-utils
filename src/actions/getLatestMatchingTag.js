1493412ad8705dd6a13314875ad77344d1b49f9b
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-07-31 11:58:43 +0200

    =Write actions in js instead of ts

---
+++ src/actions/getLatestMatchingTag.js
@@ -0,0 +1,34 @@
+import * as core from '@actions/core';
+import { getOctokit } from "@actions/github";
+
+import getTags from '../helpers/githubApi/getTags';
+
+async function run() {
+  try {
+    const repository = core.getInput('repository');
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const prefix = core.getInput('prefix');
+
+    const client = getOctokit(token);
+    let page = 1;
+
+    while (true) {
+      const tags = await getTags(client, owner, repository, page);
+      const tagsStartingWithPrefix = tags.filter((tag) => tag.name.startsWith(prefix));
+      const latestTag = tagsStartingWithPrefix[0]?.name;
+      if (latestTag) {
+        core.setOutput('tag', latestTag);
+        break;
+      } else if (tags.length === 0) {
+        throw new Error(`Tag not found for the specified prefix (${prefix}) for repository ${repository}`);
+      }
+      page++;
+    }
+  } catch (error) {
+    console.error(error);
+    core.setFailed(error);
+  }
+}
+
+run();
---
+++ src/helpers/githubApi/getBranchCommit.js
@@ -0,0 +1,11 @@
+// It returns the commit sha of a branch (latest commit).
+export default async (octokit, owner, repo, branch) => {
+  // GET /repos/{owner}/{repo}/branches/{branch}
+  const { data } = await octokit.rest.repos.getBranch({
+    owner,
+    repo,
+    branch,
+  });
+
+  return data.commit.sha;
+}
