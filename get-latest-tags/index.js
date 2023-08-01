8e991c7437424840c21a7a514264c962c363080b
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-08-01 12:34:14 +0200

    EX-179 Create github composite actions to be used in the create testing candidate process (#1)
    Signed-off-by: Pau Pujolàs Parset <pau.pujolas@aizon.ai>

---
+++ get-latest-tags/index.js
@@ -0,0 +1,37 @@
+import * as core from '@actions/core';
+import { getOctokit } from "@actions/github";
+
+import getTags from '../src/helpers/githubApi/getTags.js';
+import getBranchCommit from '../src/helpers/githubApi/getBranchCommit.js';
+
+async function run() {
+  try {
+    const repositories = core.getInput('repositories').split(',');
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const branch = core.getInput('branch');
+
+    const client = getOctokit(token);
+
+    const repoTagPairs = await Promise.all(repositories.map(async (repo) => {
+      const commit = await getBranchCommit(client, owner, repo, branch);
+      const tags = await getTags(client, owner, repo);
+
+      const tagInLatestCommit = tags.find((tag) => tag.commit.sha === commit)?.name;
+
+      if (!tagInLatestCommit) {
+        throw new Error(`Tag not found in latest commit (${commit}) for repository ${repo}`);
+      }
+      
+      return [repo, tagInLatestCommit];
+    }));
+
+    const latestTags = Object.fromEntries(repoTagPairs);
+
+    core.setOutput('latestTags', latestTags);
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
+
+run();
