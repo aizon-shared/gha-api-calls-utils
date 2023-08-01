bef6f1f1879c881bfe62c7b2568b992284a47a83
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-08-01 12:34:14 +0200

    EX-179 Create github composite actions to be used in the create testing candidate process (#1)

---
+++ get-latest-matching-tag/index.js
@@ -0,0 +1,33 @@
+import * as core from '@actions/core';
+import { getOctokit } from "@actions/github";
+
+import getTags from '../src/helpers/githubApi/getTags.js';
+
+async function run() {
+  try {
+    const repository = core.getInput('repository');
+    const token = core.getInput('token');
+    const owner = core.getInput('owner');
+    const prefix = core.getInput('prefix');
+
+    const client = getOctokit(token);
+    let page = 1;
+
+    while (true) {
+      const tags = await getTags(client, owner, repository, page);
+      const tagsStartingWithPrefix = tags.filter((tag) => tag.name.startsWith(prefix));
+      const latestTag = tagsStartingWithPrefix[0]?.name;
+      if (latestTag) {
+        core.setOutput('tag', latestTag);
+        break;
+      } else if (tags.length === 0) {
+        break;
+      }
+      page++;
+    }
+  } catch (error) {
+    core.setFailed(error);
+  }
+}
+
+run();
---
+++ src/helpers/githubApi/getTags.js
@@ -0,0 +1,11 @@
+// It returns the tags of a repository.
+export default async (octokit, owner, repo, page = 1) => {
+  // GET /repos/{owner}/{repo}/tags
+  const { data } = await octokit.rest.repos.listTags({
+    owner,
+    repo,
+    page,
+  });
+
+  return data;
+}
