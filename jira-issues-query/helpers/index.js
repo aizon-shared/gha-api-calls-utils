c2cadfac4b8bde556180503f8c7c6f32bd029ca6
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-08-03 11:14:56 +0200

    Fixes in jira query action

--- jira-issues-query/helpers/index.js
+++ jira-issues-query/helpers/index.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-export const query = async (host, token, query, fields, limit, startAt) => {
+export default async (host, token, query, fields, limit, startAt) => {
   const url = `${host}/rest/api/2/search?jql=${query}&fields=${fields}&maxResults=${limit}&startAt=${startAt}`;
   const response = await fetch(url, {
     method: 'GET',
