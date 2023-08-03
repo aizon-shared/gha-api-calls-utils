f3c15dbbcdd12a3f2b84979b8eb5a88c9930280f
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-08-03 11:01:50 +0200

    Add new action to query Jira

---
+++ jira-issues-query/helpers/index.js
@@ -0,0 +1,16 @@
+import fetch from 'node-fetch';
+
+export const query = async (host, token, query, fields, limit, startAt) => {
+  const url = `${host}/rest/api/2/search?jql=${query}&fields=${fields}&maxResults=${limit}&startAt=${startAt}`;
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Authorization': `Basic ${token}`,
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await response.json();
+
+  return data;
+};
