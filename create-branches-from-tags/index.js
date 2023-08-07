7e9573b8720077f2df0bbc95cb39c4baea2152df
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-08-07 14:10:08 +0200

    EX-179 Changes in action

--- create-branches-from-tags/index.js
+++ create-branches-from-tags/index.js
@@ -16,9 +16,7 @@ async function run() {
 
     await Promise.all(repositories.map(async ([repo, tag]) => {
       const tagInfo = await getRef(client, owner, repo, `tags/${tag}`);
-      console.log(`tagInfo: ${tagInfo.object.sha}`);
       const commitInfo = await getTag(client, owner, repo, tagInfo.object.sha);
-      console.log(`commitInfo: ${commitInfo.object.sha}`);
       await createRef(client, owner, repo, `refs/heads/${branch}`, commitInfo.object.sha);
     }));
   } catch (error) {
