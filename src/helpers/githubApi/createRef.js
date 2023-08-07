b5b443f8c3ea8f28d5be3d9f2f24c256b7008a7a
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-08-07 12:10:32 +0200

    EX-179 Changes in action

--- src/helpers/githubApi/createRef.js
+++ src/helpers/githubApi/createRef.js
@@ -1,6 +1,5 @@
 // Creates a ref in the repository.
 export default async (octokit, owner, repo, ref, sha) => {
-  console.log(`Creating ref ${ref} in ${owner}/${repo} pointing to ${sha}`);
   // POST /repos/{owner}/{repo}/git/refs
   await octokit.rest.git.createRef({
     owner,
