e7d21d4c9ada2990c9ef69d080f4517b1325d2e0
Pau Pujolàs Parset <pau.pujolas@aizon.ai>
2023-09-15 18:26:13 +0200

    fix: EX-179 take into account Lightweight and Annotated tags (#13)

--- create-branches-from-tags/index.js
+++ create-branches-from-tags/index.js
@@ -11,12 +11,6 @@ async function run() {
     const client = getOctokit(token);
 
     await Promise.all(repositories.map(async ([repo, tag]) => {
-      const { data: tagInfo } = await client.rest.git.getRef({
-        owner,
-        repo,
-        ref: `tags/${tag}`,
-      });
-
       try {
         await client.rest.git.getRef({
           owner,
@@ -25,16 +19,38 @@ async function run() {
         });
         core.info(`Skipping ${repo} because branch ${branch} already exists`);
       } catch (error) {
+        let commitSha;
+
         if (error.status === 404) {
-          core.info(`Creating branch ${branch} from in sha ${tagInfo.object.sha} in ${repo}`);
+          const { data: tagInfo } = await client.rest.git.getRef({
+            owner,
+            repo,
+            ref: `tags/${tag}`,
+          });
+
+          if (tagInfo.object.type === 'tag') {
+            // Annotated tag
+            console.log('annotated tag')
+            const { data: annotatedTagInfo } = await client.rest.git.getTag({
+              owner,
+              repo,
+              tag_sha: tagInfo.object.sha,
+            });
+
+            commitSha = annotatedTagInfo.object.sha;
+          } else if (tagInfo.object.type === 'commit') {
+            // Lightweight tag
+            commitSha = tagInfo.object.sha;
+          }
+
+          core.info(`Creating branch ${branch} from commit ${commitSha} in ${repo}`);
           await client.rest.git.createRef({
             owner,
             repo,
             ref: `refs/heads/${branch}`,
-            sha: tagInfo.object.sha,
+            sha: commitSha,
           });
         } else {
-          core.error(error);
           throw error;
         }
       }
